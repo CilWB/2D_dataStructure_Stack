93c2a7127ea09f12f81488b2c9579eb58b67a673
WB <60010933@kmitl.ac.th>
2018-09-16 20:45:49 +0700

    Implemented Stack class

--- 2D_dataStructure_Stack/_2D_dataStructure_Stack.py
+++ 2D_dataStructure_Stack/_2D_dataStructure_Stack.py
@@ -1,1 +1,35 @@
+class Stack:
+    """
+                                                         _________________________   
+                    /\\\\      _____          _____       |   |     |     |    | |  \\  
+     ,-----,       /  \\\\____/__|__\_    ___/__|__\___   |___|_____|_____|____|_|___\\ 
+  ,--'---:---`--, /  |  _     |     `| |      |      `| |                    | |    \\
+ ==(o)-----(o)==J    `(o)-------(o)=   `(o)------(o)'   `--(o)(o)--------------(o)--'  
+`````````````````````````````````````````````````````````````````````````````````````    
+    """
 
+    def __init__(self,i = None):
+        self.items = [] if i == None else i
+
+    def __str__(self):
+        return 'stack: size= '+ str(self.size()) + ' =>> '+ str(self.items)
+
+    def size(self):
+        return len(self.items)
+
+    def pop(self):
+        return self.items.pop()
+
+    def push(self,data):
+        self.items.append(data)
+
+    def peek(self):
+        return self.items[-1]
+
+s = Stack([i for i in range(10)])
+print(s)
+print(s.peek())
+print(s.pop())
+print(s)
+s.push('Toyota Vios haha+')
+print(s)
